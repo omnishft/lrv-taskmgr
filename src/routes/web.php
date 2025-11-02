2b1c973f9d05f88c47673c5cf294b58443762e4a
omnishift <c.urselita@gmail.com>
2025-11-02 02:38:27 +0100

    Refactoring: Route Model Binding completed

--- src/routes/web.php
+++ src/routes/web.php
@@ -37,7 +37,7 @@ Route::post('/tasks', function (Request $request) {
     $task->long_description = $data['long_description'];
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created succesfully!');
 })->name('tasks.store');
 
@@ -54,7 +54,7 @@ Route::put('/tasks/{task}/edit', function (Task $task, Request $request) {
     $task->long_description = $data['long_description'];
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated succesfully!');
 })->name('tasks.update');
 
