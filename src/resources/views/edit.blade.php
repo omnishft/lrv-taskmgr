ddda22ec25cf2f6c9414cd159e8cac8667a07eee
omnishift <c.urselita@gmail.com>
2025-11-04 15:59:47 +0100

    refactor: added form subview

--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -1,36 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Tasks')
 
 @section('content')
-<!-- {{ $errors }} -->
-<form method="POST" action="{{route('tasks.update', ['task' => $task->id])}}">
-    @csrf
-    @method('PUT')
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" task="title" value="{{$task->title}}"/>
-        @error('title')
-            <p class="error-message">{{$message}}</p>
-        @enderror
-
-    </div>
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" task="description" rows="5">{{$task->description}}</textarea>
-        @error('description')
-            <p class="error-message">{{$message}}</p>
-        @enderror
-    </div>
-    <div>
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" task="long_description" rows="5">{{$task->long_description}}</textarea>
-        @error('long_description')
-            <p class="error-message">{{$message}}</p>
-        @enderror
-    </div>
-    <div>
-        <button type="submit">Add Task</button>
-    </div>
-</form>
+    @include('form')
 @endsection
